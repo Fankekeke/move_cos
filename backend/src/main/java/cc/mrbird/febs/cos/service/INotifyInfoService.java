8e614b29b8a7bd8d366b3ec600c9c5e64d66cc9f
fanke <1258846421@qq.com>
2023-02-16 16:08:21 +0800

    fix -后端初始化

--- backend/src/main/java/cc/mrbird/febs/cos/service/INotifyInfoService.java
+++ backend/src/main/java/cc/mrbird/febs/cos/service/INotifyInfoService.java
@@ -1,11 +1,24 @@
 package cc.mrbird.febs.cos.service;
 
 import cc.mrbird.febs.cos.entity.NotifyInfo;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.IService;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.LinkedHashMap;
 
 /**
  * @author FanK
  */
 public interface INotifyInfoService extends IService<NotifyInfo> {
 
+    /**
+     * 分页获取通知信息
+     *
+     * @param page       分页对象
+     * @param notifyInfo 通知信息
+     * @return 结果
+     */
+    IPage<LinkedHashMap<String, Object>> selectNotifyPage(Page<NotifyInfo> page, NotifyInfo notifyInfo);
 }
--- backend/src/main/java/cc/mrbird/febs/cos/service/impl/NotifyInfoServiceImpl.java
+++ backend/src/main/java/cc/mrbird/febs/cos/service/impl/NotifyInfoServiceImpl.java
@@ -3,8 +3,12 @@ package cc.mrbird.febs.cos.service.impl;
 import cc.mrbird.febs.cos.entity.NotifyInfo;
 import cc.mrbird.febs.cos.dao.NotifyInfoMapper;
 import cc.mrbird.febs.cos.service.INotifyInfoService;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
+
+import java.util.LinkedHashMap;
 
 /**
  * @author FanK
@@ -12,4 +16,15 @@ import org.springframework.stereotype.Service;
 @Service
 public class NotifyInfoServiceImpl extends ServiceImpl<NotifyInfoMapper, NotifyInfo> implements INotifyInfoService {
 
+    /**
+     * 分页获取通知信息
+     *
+     * @param page       分页对象
+     * @param notifyInfo 通知信息
+     * @return 结果
+     */
+    @Override
+    public IPage<LinkedHashMap<String, Object>> selectNotifyPage(Page<NotifyInfo> page, NotifyInfo notifyInfo) {
+        return baseMapper.selectNotifyPage(page, notifyInfo);
+    }
 }
